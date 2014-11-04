d3bc79920b1ec793307494301db1b3017202cae3
AubreyNations <aubreynations@yahoo.com>
2014-11-04 15:50:00 -0600

    progress

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,8 +14,8 @@
 from Tkinter import *
 root = Tk()
 
-drawpad = Canvas(root, width=800,height=600, background='white')
-player = drawpad.create_oval(390,580,410,600, fill="red")
+drawpad = Canvas(root, width=800,height=600, background='#C2FCFF')
+player = drawpad.create_oval(390,580,410,600, fill="#CA85FF")
 
 # Create your "enemies" here, before the class
 
@@ -27,11 +27,25 @@ class MyApp:
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
        	    self.up = Button(self.myContainer1)
-       	    self.up.configure(text="up", background= "green")
-       	    self.up.grid(row=0,column=0)
-       	    # Bind an event to the first button
+       	    self.up.configure(text="Up", background= "#8AD6FF")
+       	    self.up.grid(row=0,column=1)
        	    self.up.bind("<Button-1>", self.upClicked)
        	    
+       	    self.left = Button(self.myContainer1)
+	    self.left.configure(text="Left", background= "#8AD6FF")
+	    self.left.grid(row=1,column=0)
+	    self.left.bind("<Button-1>", self.leftClicked)
+		
+	    self.right = Button(self.myContainer1)
+	    self.right.configure(text="Right", background= "#8AD6FF")
+	    self.right.grid(row=1,column=2)											
+	    self.right.bind("<Button-1>", self.rightClicked)
+            
+            self.down = Button(self.myContainer1)
+	    self.down.configure(text="Down", background= "#8AD6FF")
+	    self.down.grid(row=2,column=1)											
+	    self.down.bind("<Button-1>", self.downClicked) 
+	         	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=BOTTOM)
 	
@@ -45,7 +59,21 @@ class MyApp:
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
+
+        def leftClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,-20,0)
+	   
+	def rightClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,20,0)		
 		
+	def downClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,0,20)
 		
 app = MyApp(root)
 root.mainloop()
