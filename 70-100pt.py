b288e776656932f89c1ff7e27563252ad72f40c0
Aubrey Nations <aubreynations@yahoo.com>
2014-11-04 22:00:40 -0600

    progress (Help needed!)

--- 70-100pt.py
+++ 70-100pt.py
@@ -10,13 +10,15 @@
 # Make them scroll at different speeds and directions.
 
 
-
+import random
 from Tkinter import *
 root = Tk()
 
 drawpad = Canvas(root, width=800,height=600, background='#C2FCFF')
 player = drawpad.create_oval(390,580,410,600, fill="#CA85FF", outline="#CA85FF")
-enemy1 = drawpad.create_rectangle(0,100,125,125, fill="#FF9D1C", outline="#FF9D1C")
+enemy1 = drawpad.create_rectangle(random.randint(10,250),100,125,125, fill="#FF9D1C", outline="#FF9D1C")
+enemy2 = drawpad.create_rectangle(random.randint(10,250),250,125,275, fill="#FF9D1C", outline="#FF9D1C")
+enemy3 = drawpad.create_rectangle(random.randint(10,250),400,125,425, fill="#FF9D1C", outline="#FF9D1C")
 direction = 1
 # Create your "enemies" here, before the class
 
@@ -81,11 +83,34 @@ def animate():
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(enemy1)
     if x2 > drawpad.winfo_width():
-        direction = - 810
+        direction = - 825
     elif x1 < -5:
         direction = 5
     #Move our oval object by the value of direction
     drawpad.move(enemy1,direction,0)
+    drawpad.after(1, animate)
+    
+def animate():
+    global direction    
+    x1, y1, x2, y2 = drawpad.coords(enemy2)
+    if x2 > drawpad.winfo_width():
+        direction = - 825
+    elif x1 < -5:
+        direction = 5
+    #Move our oval object by the value of direction
+    drawpad.move(enemy2,direction,0)
+    drawpad.after(1, animate)
+    
+def animate():
+    global direction    
+    x1, y1, x2, y2 = drawpad.coords(enemy3)
+    if x2 > drawpad.winfo_width():
+        direction = - 825
+    elif x1 < -5:
+        direction = 5
+    #Move our oval object by the value of direction
+    drawpad.move(enemy3,direction,0)
+    
     # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
